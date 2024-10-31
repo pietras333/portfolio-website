4654353596c48d56f29d75f01bc76e9eeadbde19
Piotr Wendt <wendtpiotr333@gmail.com>
2024-10-31 22:38:01 +0100

    feat: make resume page responsive, finish resume page

---
+++ src/Pages/Projects.jsx
@@ -0,0 +1,12 @@
+const Projects = () => {
+  return (
+    <section
+      id="resume"
+      className="w-full h-screen flex flex-col bg-[#ebebeb] relative"
+    >
+      hello
+    </section>
+  );
+};
+
+export default Projects;
--- src/Pages/Resume.jsx
+++ src/Pages/Resume.jsx
@@ -3,18 +3,22 @@ import CodeLogo from "../Assets/Icons/code-logo.png";
 import CodecademyLogo from "../Assets/Icons/codecademy-logo.png";
 import ZSTLogo from "../Assets/Icons/zst-logo.png";
 import DownloadIcon from "../Assets/Icons/download.png";
+import Arrow from "../Assets/arrow.png";
 
 const Resume = () => {
   return (
-    <section className="w-full h-screen flex flex-col bg-[#ebebeb]">
-      <h2 className="text-4xl font-light flex flex-col justify-center items-center h-[20%] leading-snug bg-[#ebebeb]">
+    <section
+      id="resume"
+      className="w-full h-screen max-xl:h-fit flex flex-col bg-[#ebebeb] relative"
+    >
+      <h2 className="text-4xl font-light flex flex-col justify-center items-center h-[20%] max-xl:h-[5%] leading-snug bg-[#ebebeb]">
         <h3 className="font-normal opacity-50 tracking-widest text-2xl">
           Wendt Piotr
         </h3>
         Résumé
       </h2>
-      <section className="w-full h-[80%] flex max-xl:flex-col-reverse">
-        <section className="w-1/5 h-full flex flex-col p-4 text-[#303030]">
+      <section className="w-full h-[80%] flex max-xl:flex-col">
+        <section className="w-1/5 h-full flex flex-col max-xl:w-full p-4 text-[#303030]">
           <h2 className="text-3xl opacity-70 h-[10%]">Education</h2>
           <h3 className="w-full text-xl">
             Zespół Szkół Technicznych <br /> im. Hipolita Cegielskiego w Pile
@@ -32,17 +36,17 @@ const Resume = () => {
             <li className="opacity-60">Proficency in English Language</li>
           </ol>
         </section>
-        <section className="w-1/5 h-full flex flex-col p-4 text-[#303030] ">
+        <section className="w-1/5 h-full flex flex-col max-xl:w-full p-4 text-[#303030] ">
           <h2 className="text-3xl opacity-70 h-[10%]">Certifications</h2>
-          <caption className="font-light text-sm opacity-50 w-full text-left">
+          <caption className="max-xl:hidden font-light text-sm opacity-50 w-full text-left">
             Hover & Click to download the documents
           </caption>
-          <ol className="flex flex-col w-full h-[90%]">
+          <ol className="flex flex-col w-full h-[90%] ">
             <li className="flex w-5/6 items-center ">
               <img
                 src={CodecademyLogo}
                 alt="Codecademy Logo"
-                className="w-[60px] h-[60px] opacity-50"
+                className="w-[60px] h-[60px] opacity-50 max-xl:h-[40px] max-xl:w-[40px]"
               />
               <p className="text-wrap text-base ml-2">
                 <span className="font-bold">2024&nbsp;</span>Fundamentals Of
@@ -54,7 +58,7 @@ const Resume = () => {
               <img
                 src={CodecademyLogo}
                 alt="Codecademy Logo"
-                className="w-[60px] h-[60px] opacity-50"
+                className="w-[60px] h-[60px] opacity-50 max-xl:h-[40px] max-xl:w-[40px]"
               />
               <p className="text-wrap text-base ml-2">
                 <span className="font-bold">2024&nbsp;</span>Introduction to
@@ -65,7 +69,7 @@ const Resume = () => {
               <img
                 src={CodecademyLogo}
                 alt="Codecademy Logo"
-                className="w-[60px] h-[60px] opacity-50"
+                className="w-[60px] h-[60px] opacity-50 max-xl:h-[40px] max-xl:w-[40px]"
               />
               <p className="text-wrap text-base ml-2">
                 <span className="font-bold">2024&nbsp;</span>Learn C# Course
@@ -75,7 +79,7 @@ const Resume = () => {
               <img
                 src={CodecademyLogo}
                 alt="Codecademy Logo"
-                className="w-[60px] h-[60px] opacity-50"
+                className="w-[60px] h-[60px] opacity-50 max-xl:h-[40px] max-xl:w-[40px]"
               />
               <p className="text-wrap text-base ml-2">
                 <span className="font-bold">2024&nbsp;</span>Learn Git & GitHub
@@ -86,7 +90,7 @@ const Resume = () => {
               <img
                 src={CodecademyLogo}
                 alt="Codecademy Logo"
-                className="w-[60px] h-[60px] opacity-50"
+                className="w-[60px] h-[60px] opacity-50 max-xl:h-[40px] max-xl:w-[40px]"
               />
               <p className="text-wrap text-base ml-2">
                 <span className="font-bold">2024&nbsp;</span>Learn How To Code
@@ -97,7 +101,7 @@ const Resume = () => {
               <img
                 src={CiscoLogo}
                 alt="Cisco academy Logo"
-                className="w-[60px] h-[60px] opacity-50"
+                className="w-[60px] h-[60px] opacity-50 max-xl:h-[40px] max-xl:w-[40px]"
               />
               <p className="text-wrap text-base ml-2">
                 <span className="font-bold">2023&nbsp;</span>Introduction to
@@ -108,7 +112,7 @@ const Resume = () => {
               <img
                 src={ZSTLogo}
                 alt="ZST School Logo"
-                className="w-[60px] h-[60px] opacity-50"
+                className="w-[60px] h-[60px] opacity-50 max-xl:h-[40px] max-xl:w-[40px]"
               />
               <p className="text-wrap text-base ml-2">
                 <span className="font-bold">2022&nbsp;</span>Introduction to IoT
@@ -118,7 +122,7 @@ const Resume = () => {
               <img
                 src={CiscoLogo}
                 alt="Cisco academy Logo"
-                className="w-[60px] h-[60px] opacity-50"
+                className="w-[60px] h-[60px] opacity-50 max-xl:h-[40px] max-xl:w-[40px]"
               />
               <p className="text-wrap text-base ml-2">
                 <span className="font-bold">2024&nbsp;</span>Learn-A-Thon
@@ -127,173 +131,173 @@ const Resume = () => {
             </li>
           </ol>
         </section>
-        <section className="w-2/5 h-full flex flex-col p-4 text-[#303030] ">
+        <section className="w-2/5 h-full flex flex-col p-4 max-xl:w-full text-[#303030] ">
           <h2 className="text-3xl opacity-70 h-[10%]">Skills & Competenties</h2>
-          <ol className="flex flex-col w-full h-[90%] flex-wrap">
-            <li className="flex w-1/2 items-center h-[60px]">
+          <ol className="flex flex-col w-full h-[90%] max-xl:h-full flex-wrap max-xl:flex-nowrap max-xl:justify-center max-xl:items-center">
+            <li className="flex w-1/2 items-center h-[60px] max-xl:w-full">
               <img
                 src={CodeLogo}
                 alt="Code Logo"
-                className="w-[60px] h-[60px] opacity-50"
+                className="w-[60px] h-[60px] opacity-50 max-xl:h-[40px] max-xl:w-[40px]"
               />
               <p className="text-wrap text-base ml-2">
                 Programming Languages (Javascript, C#, Python)
               </p>
             </li>
-            <li className="flex w-1/2 items-center h-[60px]">
+            <li className="flex w-1/2 items-center h-[60px] max-xl:w-full">
               <img
                 src={CodeLogo}
                 alt="Code Logo"
-                className="w-[60px] h-[60px] opacity-50"
+                className="w-[60px] h-[60px] opacity-50 max-xl:h-[40px] max-xl:w-[40px]"
               />
               <p className="text-wrap text-base ml-2">
                 Version Control <br /> (Git, GitHub)
               </p>
             </li>
-            <li className="flex w-1/2 items-center h-[60px]">
+            <li className="flex w-1/2 items-center h-[60px] max-xl:w-full">
               <img
                 src={CodeLogo}
                 alt="Code Logo"
-                className="w-[60px] h-[60px] opacity-50"
+                className="w-[60px] h-[60px] opacity-50 max-xl:h-[40px] max-xl:w-[40px]"
               />
               <p className="text-wrap text-base ml-2">Object-Oriented Design</p>
             </li>
-            <li className="flex w-1/2 items-center h-[60px]">
+            <li className="flex w-1/2 items-center h-[60px] max-xl:w-full">
               <img
                 src={CodeLogo}
                 alt="Code Logo"
-                className="w-[60px] h-[60px] opacity-50"
+                className="w-[60px] h-[60px] opacity-50 max-xl:h-[40px] max-xl:w-[40px]"
               />
               <p className="text-wrap text-base ml-2">
                 Database Design & Managment <br />
                 (MySQL, MongoDB)
               </p>
             </li>
-            <li className="flex w-1/2 items-center h-[60px]">
+            <li className="flex w-1/2 items-center h-[60px] max-xl:w-full">
               <img
                 src={CodeLogo}
                 alt="Code Logo"
-                className="w-[60px] h-[60px] opacity-50"
+                className="w-[60px] h-[60px] opacity-50 max-xl:h-[40px] max-xl:w-[40px]"
               />
               <p className="text-wrap text-base ml-2">
                 Network Design & Managment
               </p>
             </li>
-            <li className="flex w-1/2 items-center h-[60px]">
+            <li className="flex w-1/2 items-center h-[60px] max-xl:w-full">
               <img
                 src={CodeLogo}
                 alt="Code Logo"
-                className="w-[60px] h-[60px] opacity-50"
+                className="w-[60px] h-[60px] opacity-50 max-xl:h-[40px] max-xl:w-[40px]"
               />
               <p className="text-wrap text-base ml-2">
                 Hardware Repair & Composement
               </p>
             </li>
-            <li className="flex w-1/2 items-center h-[60px]">
+            <li className="flex w-1/2 items-center h-[60px] max-xl:w-full">
               <img
                 src={CodeLogo}
                 alt="Code Logo"
-                className="w-[60px] h-[60px] opacity-50"
+                className="w-[60px] h-[60px] opacity-50 max-xl:h-[40px] max-xl:w-[40px]"
               />
               <p className="text-wrap text-base ml-2">
                 Network Security Managment
               </p>
             </li>
-            <li className="flex w-1/2 items-center h-[60px]">
+            <li className="flex w-1/2 items-center h-[60px] max-xl:w-full">
               <img
                 src={CodeLogo}
                 alt="Code Logo"
-                className="w-[60px] h-[60px] opacity-50"
+                className="w-[60px] h-[60px] opacity-50 max-xl:h-[40px] max-xl:w-[40px]"
               />
               <p className="text-wrap text-base ml-2">
                 Documentation, Estimation & Technical Writing
               </p>
             </li>
-            <li className="flex w-1/2 items-center h-[60px]">
+            <li className="flex w-1/2 items-center h-[60px] max-xl:w-full">
               <img
                 src={CodeLogo}
                 alt="Code Logo"
-                className="w-[60px] h-[60px] opacity-50"
+                className="w-[60px] h-[60px] opacity-50 max-xl:h-[40px] max-xl:w-[40px]"
               />
               <p className="text-wrap text-base ml-2">
                 Frameworks & Libraries <br /> (ReactJS, TailwindCSS, Express,
                 .NET)
               </p>
             </li>
-            <li className="flex w-1/2 items-center h-[60px]">
+            <li className="flex w-1/2 items-center h-[60px] max-xl:w-full">
               <img
                 src={CodeLogo}
                 alt="Code Logo"
-                className="w-[60px] h-[60px] opacity-50"
+                className="w-[60px] h-[60px] opacity-50 max-xl:h-[40px] max-xl:w-[40px]"
               />
               <p className="text-wrap text-base ml-2">
                 Backend Development <br /> (RESTful APIs)
               </p>
             </li>
-            <li className="flex w-1/2 items-center h-[60px]">
+            <li className="flex w-1/2 items-center h-[60px] max-xl:w-full">
               <img
                 src={CodeLogo}
                 alt="Code Logo"
-                className="w-[60px] h-[60px] opacity-50"
+                className="w-[60px] h-[60px] opacity-50 max-xl:h-[40px] max-xl:w-[40px]"
               />
               <p className="text-wrap text-base ml-2">
                 Unity Engine & its packages <br /> (Relay, Lobby, NGO)
               </p>
             </li>
 
-            <li className="flex w-1/2 items-center h-[60px]">
+            <li className="flex w-1/2 items-center h-[60px] max-xl:w-full">
               <img
                 src={CodeLogo}
                 alt="Code Logo"
-                className="w-[60px] h-[60px] opacity-50"
+                className="w-[60px] h-[60px] opacity-50 max-xl:h-[40px] max-xl:w-[40px]"
               />
               <p className="text-wrap text-base ml-2">Arduino basics</p>
             </li>
-            <li className="flex w-1/2 items-center h-[60px]">
+            <li className="flex w-1/2 items-center h-[60px] max-xl:w-full">
               <img
                 src={CodeLogo}
                 alt="Code Logo"
-                className="w-[60px] h-[60px] opacity-50"
+                className="w-[60px] h-[60px] opacity-50 max-xl:h-[40px] max-xl:w-[40px]"
               />
               <p className="text-wrap text-base ml-2">
                 Game development knowledge
               </p>
             </li>
-            <li className="flex w-1/2 items-center h-[60px]">
+            <li className="flex w-1/2 items-center h-[60px] max-xl:w-full">
               <img
                 src={CodeLogo}
                 alt="Code Logo"
-                className="w-[60px] h-[60px] opacity-50"
+                className="w-[60px] h-[60px] opacity-50 max-xl:h-[40px] max-xl:w-[40px]"
               />
               <p className="text-wrap text-base ml-2">
                 Basics of image Processing
               </p>
             </li>
-            <li className="flex w-1/2 items-center h-[60px]">
+            <li className="flex w-1/2 items-center h-[60px] max-xl:w-full">
               <img
                 src={CodeLogo}
                 alt="Code Logo"
-                className="w-[60px] h-[60px] opacity-50"
+                className="w-[60px] h-[60px] opacity-50 max-xl:h-[40px] max-xl:w-[40px]"
               />
               <p className="text-wrap text-base ml-2">
                 Windows & Linux Server Knowledge
               </p>
             </li>
-            <li className="flex w-1/2 items-center h-[60px]">
+            <li className="flex w-1/2 items-center h-[60px] max-xl:w-full">
               <img
                 src={CodeLogo}
                 alt="Code Logo"
-                className="w-[60px] h-[60px] opacity-50"
+                className="w-[60px] h-[60px] opacity-50 max-xl:h-[40px] max-xl:w-[40px]"
               />
               <p className="text-wrap text-base ml-2">
                 Windows & Linux OS Knowledge
               </p>
             </li>
-            <li className="flex w-1/2 items-center h-[60px]">
+            <li className="flex w-1/2 items-center h-[60px] max-xl:w-full">
               <img
                 src={CodeLogo}
                 alt="Code Logo"
-                className="w-[60px] h-[60px] opacity-50"
+                className="w-[60px] h-[60px] opacity-50 max-xl:h-[40px] max-xl:w-[40px]"
               />
               <p className="text-wrap text-base ml-2">
                 Scrum & Agile Development Methodologies
@@ -301,7 +305,7 @@ const Resume = () => {
             </li>
           </ol>
         </section>
-        <section className="w-1/5 h-full flex flex-col p-4 items-start text-[#303030]">
+        <section className="w-1/5 h-full flex flex-col p-4 items-start max-xl:w-full text-[#303030]">
           <h2 className="text-3xl opacity-70 h-[10%]">Soft Skills</h2>
           <ol className="flex flex-col text-xl h-[90%]">
             <li className="opacity-60">Problem Solving</li>
@@ -312,6 +316,13 @@ const Resume = () => {
           </ol>
         </section>
       </section>
+      <button className=" max-xl:hidden opacity-50 hover:opacity-100 absolute left-16 bottom-16 max-lg:right-8 max-md:text-sm tracking-widest max-lg:w-[200px] flex flex-col justify-center items-center">
+        <img
+          src={Arrow}
+          alt="arrow down"
+          className="w-[50px] h-[50px] max-md:w-[40px] max-md:h-[40px] max-lg:w-[50px] max-lg:h-[50px] "
+        />
+      </button>
     </section>
   );
 };
